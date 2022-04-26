4644a6ad1d47fcca362b9bbc654409eda49c5fbe
Tariselan <woodmoreaaron@gmail.com>
2022-04-27 00:55:21 +1200

    Began My Mental Breakdown by Adding JS

---
+++ nouns.js
@@ -0,0 +1,8 @@
+var score1 = 0;
+function scoreInc() {
+    score1++;
+    document.getElementById("score1").innerHTML = score1;
+}
+function game1check() {
+    let x = document.getElementById("input1").innerText;
+}
